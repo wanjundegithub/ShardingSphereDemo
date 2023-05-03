aec50bfd7dd0bd1356950a9407a1ee7782329164
wanjundegithub <2304564274@qq.com>
2023-05-03 20:44:09 +0800

    完成分库分表插入

--- src/main/resources/tabel.sql
+++ src/main/resources/tabel.sql
@@ -1,4 +1,12 @@
 create database `ds0`;
+CREATE TABLE `ds0`.`t_order0` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`user_id` int(11) DEFAULT NULL,
+`order_id` int(11) DEFAULT NULL,
+`cloumn` varchar(45) DEFAULT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `ds0`.`t_order1` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `user_id` int(11) DEFAULT NULL,
@@ -7,16 +15,8 @@ CREATE TABLE `ds0`.`t_order1` (
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `ds0`.`t_order2` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
-`user_id` int(11) DEFAULT NULL,
-`order_id` int(11) DEFAULT NULL,
-`cloumn` varchar(45) DEFAULT NULL,
-PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 create database `ds1`;
-CREATE TABLE `ds1`.`t_order1` (
+CREATE TABLE `ds1`.`t_order0` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `user_id` int(11) DEFAULT NULL,
 `order_id` int(11) DEFAULT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE `ds1`.`t_order1` (
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `ds1`.`t_order2` (
+CREATE TABLE `ds1`.`t_order1` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `user_id` int(11) DEFAULT NULL,
 `order_id` int(11) DEFAULT NULL,
